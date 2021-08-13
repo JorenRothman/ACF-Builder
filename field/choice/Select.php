f7aac9d935d8c76c72cb6b0fc6592b665646b8b6
Joren Rothman <joren@instance.studio>
2021-08-13 12:37:42 +0200

    Bugfix missing UI on select field

--- field/choice/Select.php
+++ field/choice/Select.php
@@ -3,12 +3,13 @@
 namespace ACFBuilder\Field\Choice;
 
 use ACFBuilder\Field\Field;
+use ACFBuilder\Field\Helpers\UI;
 use ACFBuilder\Field\Helpers\Ajax;
-use ACFBuilder\Field\Helpers\AllowNull;
 use ACFBuilder\Field\Helpers\Choices;
-use ACFBuilder\Field\Helpers\DefaultValue;
 use ACFBuilder\Field\Helpers\Multiple;
+use ACFBuilder\Field\Helpers\AllowNull;
 use ACFBuilder\Field\Helpers\Placeholder;
+use ACFBuilder\Field\Helpers\DefaultValue;
 use ACFBuilder\Field\Helpers\ReturnFormat;
 
 /**
@@ -23,6 +24,7 @@ class Select extends Field
     use AllowNull;
     use Ajax;
     use Multiple;
+    use UI;
 
     protected function setType()
     {
