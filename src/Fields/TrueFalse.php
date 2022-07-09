7ea37283fba0e613e1ff629b1526397c460f33ba
Joren Rothman <joren@instance.studio>
2022-07-09 11:48:10 +0200

    Added True False Field

---
+++ src/Fields/TrueFalse.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Joren\ACFBuilder\Fields;
+
+use Joren\ACFBuilder\Field;
+
+class TrueFalse extends Field
+{
+    public bool $default_value = false;
+
+    public string $message = '';
+
+    public bool $ui = false;
+
+    public string $ui_on_text = '';
+
+    public string $ui_off_text = '';
+
+    public function __construct(string $label, ?string $name = null, ?string $key = null)
+    {
+        parent::__construct($label, $name, $key);
+    }
+
+    public function setType(): void
+    {
+        $this->type = 'true_false';
+    }
+
+    public function setDefaultValue(bool $value): self
+    {
+        $this->default_value = $value;
+
+        return $this;
+    }
+
+    public function setMessage(string $value): self
+    {
+        $this->message = $value;
+
+        return $this;
+    }
+
+    public function setUI(bool $value): self
+    {
+        $this->ui = $value;
+
+        return $this;
+    }
+
+    public function setUiOnText(string $value): self
+    {
+        $this->ui_on_text = $value;
+
+        return $this;
+    }
+
+    public function setUiOffText(string $value): self
+    {
+        $this->ui_off_text = $value;
+
+        return $this;
+    }
+}
---
+++ tests/Fields/TrueFalseTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use Joren\ACFBuilder\Fields\TrueFalse;
+use PHPUnit\Framework\TestCase;
+
+
+final class TrueFalseTest extends TestCase
+{
+    public function testConstructor()
+    {
+        $field = new TrueFalse('A nice Label NAME', 'Name', 'Key');
+
+        $this->assertEquals('A nice Label NAME', $field->label);
+        $this->assertEquals('name', $field->name);
+        $this->assertEquals('897356954c2cd3d41b221e3f24f99bba', $field->key);
+        $this->assertEquals('true_false', $field->type);
+
+        $field = new TrueFalse('A nice Label NAME');
+
+        $this->assertEquals('A nice Label NAME', $field->label);
+        $this->assertEquals('a_nice_label_name', $field->name);
+        $this->assertEquals('9cce016c55ac67d739003eadc94d547e', $field->key);
+        $this->assertEquals('true_false', $field->type);
+    }
+}
