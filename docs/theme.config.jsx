f4908464ebec8fae573ea1a7cefa75c3e1e81d1d
Joren Rothman <joren@instance.studio>
2023-11-17 20:06:25 +0100

    Add ACF Builder logo and update project link

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -1,7 +1,14 @@
 export default {
-    logo: <span>My Nextra Documentation</span>,
+    logo: <span>ACF Builder</span>,
     project: {
-        link: "https://github.com/shuding/nextra",
+        link: "https://github.com/JorenRothman/ACF-Builder",
+    },
+    navigation: {
+        prev: true,
+        next: true,
+    },
+    footer: {
+        text: <span>MIT {new Date().getFullYear()} © ACF Builder.</span>,
     },
     // ... other theme options
 };
